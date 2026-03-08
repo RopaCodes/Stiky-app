fc874105b37df65ad4903d46e6efacdd1b09022f
Ropa <ropacodezz@gmail.com>
2026-03-08 20:34:25 +1300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,73 @@
 const toggle_btn = document.getElementById('toggle_btn')
 const toggle_bar = document.getElementById('toggle_bar')
+const notesContainer = document.getElementById("app")
+
+getNotes().forEach(note => {
+    const noteElement = createNoteElement(note.id, note.content)
+    notesContainer.appendChild(noteElement)
+    //                the note text area and + btn above
+})
 
 // Add a click event listener to the icon
 toggle_btn.addEventListener('click', () => {
     // Toggle the 'show' class on the div
     toggle_bar.classList.toggle('hidden');
-});
+})
+
+toggle_btn.addEventListener("click", () => addNote())
+
+function getNotes(){
+    // retreive existing notes from storage, calling local storage api
+    return JSON.parse(localStorage.getItem("stickynotes-notes")||"[]")
+}
+function saveNotes(notes){
+    //save local notes from browser to local storage
+    // takes in JS notes and stringifys it as a json b4 saving to local storage key
+    localStorage.setItem("stickynotes-notes", JSON.stringify(notes))
+}
+function createNoteElement(id, content){
+    //allows us to create a new text area and title element
+    //const element_title = document.createElement()
+    const element = document.createElement("textarea");
+    element.classList.add("note")
+    element.value = content;
+    element.placeholder = "Type your notes here"
+    // update the note contents in local storage
+    element.addEventListener("change",()=>{
+        updateNote(id, element.value)
+    })
+    element.addEventListener("dblclick", ()=>{
+        const doDelete = confirm("Are you sure you wish to delete this Sticky note?")
+        if (doDelete){
+            deleteNote(id, element)
+        }
+    })
+    return element
+}
+function addNote(){
+    // add note to html and local storage
+    const notes = getNotes()
+    const noteObj = {
+        id: Math.floor(Math.random()*100000),
+        content: ""
+    }
+    const noteElement = createNoteElement(noteObj.id, noteObj.content)
+    notesContainer.appendChild(noteElement)
+    notes.push(noteObj)
+    saveNotes(notes)
+
+}
+function updateNote(id, newContent){
+    const notes = getNotes()
+    const note = notes.find(n=>n.id === id)
+    if (note){ //check required incase finding doesnt find anything
+        note.content = newContent
+        saveNotes(notes)
+    }
+    
+}
+function deleteNote(id, element){
+    const notes = getNotes().filter(n => n.id !== id);
+    saveNotes(notes);
+    notesContainer.removeChild(element)
+}
